e1e425333e91cb9b9d14861cd9290b0f493a8427
ghst659 <gst659@gmail.com>
2021-09-18 03:48:52 -0700

    Variable readability fixes.

--- shudu_main.py
+++ shudu_main.py
@@ -5,7 +5,7 @@ import logging
 import sys
 import typing
 
-import board
+from board import Board, Symbol
 
 def main(argv: typing.Sequence[str]) -> int:
     """Working harness for shudu."""
@@ -17,11 +17,11 @@ def main(argv: typing.Sequence[str]) -> int:
                         dest='verbose', action="store_true",
                         help="run verbosely")
     args = parser.parse_args(args=argv[1:])
-    b = board.Board()
-    b.from_json(args.json_path)
-    print(b)
+    puzzle = Board()
+    puzzle.from_json(args.json_path)
+    print(puzzle)
     try:
-        solution = solve(b)
+        solution = solve(puzzle)
     except NoMovesCondition:
         logging.warning("no solution found")
         return 1
@@ -37,41 +37,41 @@ class NoMovesCondition(Exception):
     """Indicates that no moves are available."""
     pass
 
-def solve(b: board.Board) -> board.Board:
+def solve(current: Board) -> Board:
     """Finds a solution to the puzzle."""
     try:
-        r, c = first_empty(b)
+        row, col = first_empty(current)
     except BoardFullCondition:
-        logging.info("no empty cell")
-        return b
+        logging.info("board is full")
+        return current
     else:
-        possibles = possible_symbols(b, r, c)
-        logging.warning("(%s, %s) possible: %s", r, c, possibles)
-        n = b.clone()
-        for p in possibles:
-            n[r, c] = p
+        possibles = open_moves(current, row, col)
+        logging.info("[%s, %s] possible: %s", row, col, possibles)
+        candidate = current.clone()
+        for symbol in possibles:
+            candidate[row, col] = symbol
             try:
-                return solve(n)
+                return solve(candidate)
             except NoMovesCondition:
-                n[r, c] = board.Symbol.EMPTY
-                logging.info("[%d, %d] = %s failed", r, c, p)
-        raise NoMovesCondition(f"no moves at [{r}, {c}]")
+                candidate[row, col] = Symbol.EMPTY
+                logging.info("[%d, %d] = %s failed", row, col, symbol)
+        raise NoMovesCondition(f"no moves at [{row}, {col}]")
             
-def first_empty(b: board.Board) -> tuple[int, int]:
-    """Returns a row, column tuple for the first empty cell, else None."""
-    for r in range(9):
-        for c in range(9):
-            if not b.get(r, c):
-                return (r, c)
+def first_empty(current: Board) -> tuple[int, int]:
+    """Returns a row, column tuple for the first empty cell."""
+    for row in range(9):
+        for col in range(9):
+            if not current.get(row, col):
+                return (row, col)
     raise BoardFullCondition("no empty cell")
 
-def possible_symbols(b: board.Board, r: int, c: int) -> typing.Iterable[board.Symbol]:
-    """Returns an iterable of the possible symbols that can be inserted at (r,c)."""
-    if b[r, c]:
+def open_moves(b: Board, row: int, col: int) -> typing.Iterable[Symbol]:
+    """Returns the possible Symbols for  at [ROW, COL]."""
+    if b[row, col]:
         return frozenset()
-    row_unused = frozenset(board.Symbol.unused(b.row(r)))
-    col_unused = frozenset(board.Symbol.unused(b.col(c)))
-    box_unused = frozenset(board.Symbol.unused(b.box(r, c)))
+    row_unused = frozenset(Symbol.unused(b.row(row)))
+    col_unused = frozenset(Symbol.unused(b.col(col)))
+    box_unused = frozenset(Symbol.unused(b.box(row, col)))
     return row_unused & col_unused & box_unused
 
 if __name__ == "__main__":
